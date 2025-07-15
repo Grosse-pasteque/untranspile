154335c05653ee86bc024cdf663d83396b31b7b1
Grosse-pasteque <grossepasteque.gamer34@gmail.com>
2025-07-15 11:50:12 +0200

    small update

--- transformers/remove-helpers.js
+++ transformers/remove-helpers.js
@@ -1,14 +1,25 @@
+const t = require('@babel/types');
 const { get, list } = require("@babel/helpers");
 const { isNodesEquivalentCustom } = require('../includes/utils');
+const helpersGenerated = require('@babel/helpers/lib/helpers-generated').default;
 
-const helpers = list.map(name => {
-    const { ast } = get(name);
-    return { name, ast };
-});
+const helpers = [];
+for (const [{ ast, metadata }] of Object.entries(helpersGenerated)) {
+    if (!t.isFunctionDeclaraction(ast)) continue;
+    helpers.push()
+}
 
+// TODO: cache codemod's trees helpers with version control 
 module.exports = function(state) {
     return {
         name: "remove-helpers",
-        visitor: {}
+        visitor: {
+            FunctionDeclaration(path) {
+                const { parentPath, node } = path;
+                for ()
+                isNodesEquivalentCustom(node)
+
+            }
+        }
     };
 };
